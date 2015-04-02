d3f7a06eb379d517275ffb4f3985c1486fefecec
Nishesh <nishesh.shukla@colorado.edu>
2015-04-02 13:19:36 -0600

    added incomplete Part6 submission:

--- src/dummy_class_data.py
+++ src/dummy_class_data.py
@@ -1,5 +1,5 @@
 course_data = {
-    "course_name": "Barbie Doll Assembly",
+    "course_name": "Underwater Basket Coding",
     "assignment_types": {
         "essays": [100, 80, 90, 0],
         "homework": [100, 90, 70],
