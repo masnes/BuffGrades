cd712578027e5457e1a8ea223d96364e2c400e4d
Nishesh <nishesh.shukla@colorado.edu>
2015-04-27 11:13:35 -0600

    Added the Final Presentation, both pdf and pptx, also updated gui with dummy class grades

--- src/dummy_class_data.py
+++ src/dummy_class_data.py
@@ -1,9 +1,10 @@
 course_data = {
-    "course_name": "Barbie Doll Assembly",
+    "course_name": "Software Development Methods and Tools",
     "assignment_types": {
-        "essays": [100, 80, 90, 0],
-        "homework": [100, 90, 70],
-        "labs": [80, 50, 70]
+        "Projects": [100, 76.47, 92.30, 100, 100, 100],
+        "Exams": [100, 100, 85.71, 88.38],
+        "Homework": [100, 100, 100, 100],
+        "Labs": [100, 100, 100, 100, 100, 100, 100, 100, 100, 100]
     }
 
 }
