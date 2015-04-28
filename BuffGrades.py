e6fcb47f1f69df6c81522610ccdd7d05344fe41b
Michael Asnes <mike.asnes@gmail.com>
2015-04-27 23:08:51 -0600

    made buffgrades.py auto show the help message

--- BuffGrades.py
+++ BuffGrades.py
@@ -9,21 +9,34 @@ Usage:  python BuffGrades.py [options]
 
 Options:
     -h  print this help
+    -g  Show the gui
     -d  test the database scaffold prior to starting the gui
 ''')
 
 def main():
     run_db = False
+    show_gui = False
+    show_usage = False
+
     for arg in sys.argv[1:]: # first item in argv is the program name
         if 'h' in arg:
-            usage()
-            return
+            show_usage = True
         if 'd' in arg:
             run_db = True
+        if 'g' in arg:
+            show_gui = True
 
-    if run_db != False:
+    if show_usage == True:
+        usage()
+    elif run_db != True and show_gui != True:
+        usage()
+
+    if run_db == True:
         db.DatabaseSetup()
-    gui.run_gui()
+    if show_gui == True:
+        gui.run_gui()
+
+
 
 if __name__ == '__main__':
     main()
