ad3a7397ac8ed0b05cbda0e69e0739437164b70e
Michael Asnes <mike.asnes@gmail.com>
2015-04-27 22:50:09 -0600

    moved TestBuffGrades to test_BuffGrades: better naming for py.test

--- test/test_BuffGrades.py
+++ test/test_BuffGrades.py
@@ -5,37 +5,37 @@ from mysql.connector import errorcode
 
 import unittest
 
-from BuffGrades import DatabaseSetup, DB_NAME
-from TableData import TABLES, TABLEORDER
+from src.db import DatabaseSetup, DB_NAME
+from src.TableData import TABLES, TABLEORDER
 
 class DatabaseSetupTestMockup(DatabaseSetup):
     def __init__(self):
         """Don't automatically connect, like in the main version"""
         pass
-        
+
     def dry_connect(self):
-        """Connect to mysql without database assigned""" 
+        """Connect to mysql without database assigned"""
         cnx = mysql.connector.connect(user='root', password='pass')
         return cnx, cnx.cursor()
-        
+
     def drop_db(self, db_name):
         """Use this to clear the database for tests"""
         #TODO
         pass
-         
-    
-    
+
+
+
 class DatabaseSetupTests(unittest.TestCase):
     def setup(self):
         self.cnx = None
         self.cursor = None
         self.databaseSetupTestMockup = DatabaseSetupTestMockup()
-					 
+
     def test_connect_with_no_database_created(self):
         """Just makes sure cnx is working"""
-        cnx, cursor = DatabaseSetupTestMockup().dry_connect()        
+        cnx, cursor = DatabaseSetupTestMockup().dry_connect()
         assert cnx is not None
         assert cursor is not None
 
-	def tearDown(self):
-		pass
+    def tearDown(self):
+        pass
